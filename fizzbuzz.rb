2e26667d3fc98a2b94e34084d598b295fd3a9137
salehlayaly <lsaleh@hcstonline.org>
2018-08-07 20:07:25 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -11,7 +11,10 @@
 def fizzbuzz(int)
   if int % 3 == 0 
     puts "Fizz"
-  if int
+  elsif int % 5 == 0 
+    puts "Buzz" 
+  elsif int % 5 == 0 && int % 3 == 0 
+    puts "FizzBuzz"
 end
 
 fizzbuzz(17)
