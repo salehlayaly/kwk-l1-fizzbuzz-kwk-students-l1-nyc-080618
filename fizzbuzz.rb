0c3faefc36f4fa3c7fca8fe32d61cc1408f1bb03
salehlayaly <lsaleh@hcstonline.org>
2018-08-07 19:57:39 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,11 @@
+# define a method that takes in an argument
+# def name(argument)
+  # if a  number is divisible by 3
+  # lets put fizz
+  # if a number is divisible by 5
+  # output buzz
+  # if the number is divisible by both 
+  # output fizzbuzz
+#end your conditional and your method!!!!!
+
+def fizzbuzz
